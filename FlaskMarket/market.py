7e102252173babb3a1fb2581d126b7a3033657c3
jaleyerdelen <jaleyerdelen@gmail.com>
2022-09-14 13:17:36 +0300

    item query

--- FlaskMarket/market.py
+++ FlaskMarket/market.py
@@ -22,9 +22,5 @@ def home_page():
 
 @app.route("/market")
 def market_page():
-    items = [
-        {'id': 1, 'name': 'Phone', 'barcode': '893212299897', 'price': 500},
-        {'id': 2, 'name': 'Laptop', 'barcode': '123985473165', 'price': 900},
-        {'id': 3, 'name': 'Keyboard', 'barcode': '231985128446', 'price': 150}
-    ]
+    items = Item.query.all()
     return render_template("market.html", items=items)
