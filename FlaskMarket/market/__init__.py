af08e02022361a3afad1beef9c28fde3cbe971aa
jaleyerdelen <jaleyerdelen@gmail.com>
2022-09-19 16:46:19 +0300

    created forms

--- FlaskMarket/market/__init__.py
+++ FlaskMarket/market/__init__.py
@@ -1,7 +1,21 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
+
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///market.db"
+
+secret_key = os.getenv("SECRET_KEY")
+app.config["SECRET_KEY"] = {secret_key}
+
+def myEnvironment():
+    print(f'My key is: {secret_key}.')
+
+myEnvironment()
+
 db = SQLAlchemy(app)
 
 from market import routes
