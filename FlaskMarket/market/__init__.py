1ad604dd805649a269c2df3f3bf96682972d672d
jaleyerdelen <jaleyerdelen@gmail.com>
2022-09-20 23:28:58 +0300

    updated forms

--- FlaskMarket/market/__init__.py
+++ FlaskMarket/market/__init__.py
@@ -11,13 +11,8 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///market.db"
 secret_key = os.getenv("SECRET_KEY")
 app.config["SECRET_KEY"] = {secret_key}
 
-def myEnvironment():
-    print(f'My key is: {secret_key}.')
-
-myEnvironment()
 
 db = SQLAlchemy(app)
 
 from market import routes
-
 
--- FlaskMarket/market/forms.py
+++ FlaskMarket/market/forms.py
@@ -2,8 +2,8 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 
 class RegisterForm(FlaskForm):
-    username = StringField(label = "username")
-    email_adress = StringField(label = "email")
-    password1 = PasswordField(label = "password1")
-    password2 = PasswordField(label = "password2")
+    username = StringField(label = "User Name:")
+    email_adress = StringField(label = "Email Adress:")
+    password1 = PasswordField(label = "Password:")
+    password2 = PasswordField(label = "Confirm Password:")
     submit = SubmitField(label = "submit")
